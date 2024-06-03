07df12a248dda65b54fdcbf7cc4c89553a1a0ede
Mustard d'Joan <ionut.oltean@quadom.com>
2024-06-03 11:54:34 +0300

    has rotation and light

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,32 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   [SerializeField] private float moveSpeed = 5f;
+    [SerializeField]
+    private float _moveSpeed = 5; [SerializeField]
+    private float _rotateSpeed = 10;
+
+    private bool isWalking;
+    // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = Vector2.zero;
-        if (Input.GetKey(KeyCode.W))
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey((KeyCode.W)))
         {
-            inputVector.y = 1f;
+            inputVector.y += 1*Time.deltaTime;
         }
-
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey((KeyCode.S)))
         {
-            inputVector.y = -1f;
+            inputVector.y -= 1*Time.deltaTime;
         }
-
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey((KeyCode.D)))
         {
-            inputVector.x = -1f;
+            inputVector.x +=1*Time.deltaTime;
         }
-        
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey((KeyCode.A)))
         {
-            inputVector.x = 1f;
+            inputVector.x -= 1*Time.deltaTime;
         }
 
         inputVector = inputVector.normalized;
-        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * (Time.deltaTime * moveSpeed);
+        Vector3 moveDir =  new Vector3(inputVector.x, 0f, inputVector.y);
+        isWalking = moveDir != Vector3.zero;
+        transform.forward = Vector3.Slerp(transform.forward,moveDir * _rotateSpeed,Time.deltaTime*_rotateSpeed) ;
+        var step = moveDir * Time.deltaTime * _moveSpeed;
+        transform.position += step;
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
